43c3d84feaac000951d961b27ea580d80867ce9f
idrisakmal <simple_idris@yahoo.com>
2015-06-30 02:37:49 +0800

    added seed file to deploy to heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+	a = User.create("email" => "simple_idris@yahoo.com", "password" => "password")
+	b = Role.create("name" => "admin")
+
+	a.roles << b
+
+	
+
